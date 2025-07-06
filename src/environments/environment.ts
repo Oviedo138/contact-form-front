5ebc61278b583024b579d9f9ec8d1533c14f057c
MaximoQuin <169080723+MaximoQuin@users.noreply.github.com>
2025-07-05 21:39:03 -0600

    change enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  URL_API: "http://localhost:8080/api",
-  // URL_API: "http://mywebsite.com/",
+  URL_API: "http://24.199.88.86/api",
+  // URL_API: "http://localhost:8080/api",
 };
 
 /*
